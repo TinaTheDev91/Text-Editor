d923b5e6254c90f8ab4bccac59e71dd1e40c3512
Tina Croxton <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-19 00:15:47 -0400

    heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,26 +27,26 @@ export const putDb = async (content) => {
   const result = await request;
 
   console.log('🚀 - data saved to the database', result);
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (id) => {
   console.log('GET from the database');
 
-  const jateDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
-  const tx = jateDb.transaction('contact', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
-  const request = store.getAll();
+  const request = store.get(id);
 
   const result = await request;
   console.log('result.value', result);
   return result;
 };
 
-console.error('getDb not implemented');
+// console.error('getDb not implemented');
 
 initdb();
